0fcdad8d1689b3b27e4339f5d77962af2e7e6e8d
Thomas Ender <thomasender@Thomass-MacBook-Pro.local>
2024-06-07 10:32:38 +0200

    reverseWithSplit, reverseInt

--- reverse.js
+++ reverse.js
@@ -16,3 +16,20 @@ function reverse(str) {
 console.log(reverse("hello")); // olleh
 console.log(reverse('Test!')); // !tseT
 console.log(reverse('esrever')); // reverse
+
+function reverseWithSplit(input) {
+    return input.toString().split('').reverse().join('');
+}
+
+console.log(reverseWithSplit("hello")); // olleh
+console.log(reverseWithSplit('Test!')); // !tseT
+console.log(reverseWithSplit('esrever')); // reverse
+
+
+function reverseInt(int) {
+    return parseInt(reverseWithSplit(int)) * Math.sign(int);
+}
+
+console.log(reverseInt(123)); // 321
+console.log(reverseInt(500)); // 5
+console.log(reverseInt(-123)); // -321
