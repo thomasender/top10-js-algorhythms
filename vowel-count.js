767bfbfd0693a34c95ffd8f6748cc0df290c1151
Thomas Ender <freisingen@yahoo.com>
2024-06-08 14:10:47 +0200

    vowel-count

---
+++ vowel-count.js
@@ -0,0 +1,22 @@
+/**
+ * @params string
+ * @returns the number of vowels
+ */
+function vowelCount(str) {
+    let count = 0;
+
+    const vowels = 'aeiou'
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log("Counting Vowels in 'thomas', expect 2", vowelCount("thomas"));
+console.log("Counting Vowels in 'ffkkmm', expect 0", vowelCount("ffkkmm"));
+console.log("Counting Vowels in 'alphabet', expect 3", vowelCount("alphabet"));
+console.log("Counting Vowels in 'mississippi', expect 4", vowelCount("mississippi"));
