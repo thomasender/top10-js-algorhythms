9508fe298744a7bd49af8c136784146f7a6a29c1
Thomas Ender <freisingen@yahoo.com>
2024-06-08 13:55:15 +0200

    improve anagram-check

--- anagram-check.js
+++ anagram-check.js
@@ -3,31 +3,18 @@
  * @returns true if input strings are anagrams of each other, f.e. "tea" and "eat"
  */
 function anagramCheck(str, str2) {
-    const charMapStr = {}
-    const charMapStr2 = {}
+    const charMapStr = charMap(str)
+    const charMapStr2 = charMap(str2)
 
-    for (let char of str.toLowerCase()) {
-        if (char === ' ' || char === '?' || char === '!') continue;
-        charMapStr[char] = ++charMapStr[char] || 1;
+    
+    if (Object.keys(charMapStr).length !== Object.keys(charMapStr2).length) {
+        // if the length of the keys do not match, we do not have an anagram
+        return false
     }
 
-    for (let char of str2.toLowerCase()) {
-        if (char === ' ' || char === '?' || char === '!') continue;
-        charMapStr2[char] = ++charMapStr2[char] || 1;
-    }
-
-    // Check if charMapStr and charMapStr2 contain the exact same content
     for (key in charMapStr) {
-        if (!(charMapStr[key] && charMapStr2[key])) {
-            // The objects to not match
-            return false
-        }
-    }
-
-    // Check if charMapStr2 and charMapStr1 contain the exact same content
-    for (key in charMapStr2) {
-        if (!(charMapStr2[key] && charMapStr[key])) {
-            // The objects to not match
+        // if the keys do not match, we do not have an anagram
+        if (charMapStr[key] !== charMapStr2[key]) {
             return false
         }
     }
@@ -40,3 +27,22 @@ console.log(anagramCheck("otto", "otto")) // Expect true
 console.log(anagramCheck("Bad Credit", "Debit Card")) // Expect true
 console.log(anagramCheck("tea", "somethingelse")) // Expect false
 console.log(anagramCheck("tea", "eatnow")) // Expect false
+
+/**
+ * @params string
+ * @returns a object with:
+ *  - a key for each character of the input string
+ *  - a number as value for each key, indicating how many characters the input string contains
+ */
+function charMap(str) {
+    const charMap = {}
+
+    // Removes any spaces and punctations from the input string and transforms it to lowerCase
+    const normalizedString = str.toLowerCase().replace(/[\W]/g, '')
+    for (let char of normalizedString) {
+        // Set the number of chars found for given char
+        charMap[char] = ++charMap[char] || 1
+    }
+
+    return charMap
+}
